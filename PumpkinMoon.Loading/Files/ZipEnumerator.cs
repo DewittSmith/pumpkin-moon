ede7c39437eba45588b44e0f4bbd4ea21085688d
DewittSmith <48875693+DewittSmith@users.noreply.github.com>
2023-04-25 21:37:41 +0700

    Add zip enumerator

---
+++ PumpkinMoon.Loading/Files/ZipEnumerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace PumpkinMoon.Loading.Files;
+
+public class ZipEnumerator : IDirectoryEnumerator
+{
+    public ZipArchive Archive;
+
+    public IEnumerable<string> EnumerateFiles(string path)
+    {
+        var result = Archive.Entries.Where(x => Path.HasExtension(x.Name)).Select(x => x.FullName);
+        return result;
+    }
+
+    public IEnumerable<string> EnumerateDirectories(string path)
+    {
+        yield break;
+    }
+}
---
+++ PumpkinMoon.Loading/Files/ZipFileReader.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace PumpkinMoon.Loading.Files;
+
+public class ZipFileReader : IFileReader
+{
+    public ZipArchive Archive;
+
+    public Stream ReadFile(string path)
+    {
+        ZipArchiveEntry entry = Archive.GetEntry(path);
+        return entry?.Open();
+    }
+}
